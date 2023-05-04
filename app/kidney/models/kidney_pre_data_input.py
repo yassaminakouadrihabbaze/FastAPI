871e51b7fd6fd247ac7e46835b00677c4f622730
@yassaminakouadrihabbaze <yassaminakouadrihabbaze01@gmail.com>
2023-05-04 19:21:20 +0200

    COMPLETE ALZIHMERS DESEASE

--- app/kidney/models/kidney_pre_data_input.py
+++ app/kidney/models/kidney_pre_data_input.py
@@ -50,3 +50,55 @@ class kidney_dis_pre_input(BaseModel):
     coronary_artery_disease: int
     appetite: int
     peda_edema: int
+
+
+    #     age: Age of the patient (float) - Range: Varies depending on the dataset, typically a positive value.
+
+    # blood_pressure: Blood pressure of the patient (float) - Range: Varies depending on the dataset, typically measured in mmHg (millimeters of mercury).
+
+    # specific_gravity: Specific gravity of urine (float) - Range: Varies depending on the dataset, typically measured between 1.005 and 1.025.
+
+    # albumin: Presence of albumin in urine (float) - Range: Varies depending on the dataset, typically measured in g/dL (grams per deciliter).
+
+    # sugar: Presence of sugar in urine (float) - Range: Varies depending on the dataset, typically measured in mg/dL (milligrams per deciliter).
+
+    # red_blood_cells: Presence of red blood cells in urine (int) - Range: Usually represented as binary values (0 or 1) indicating the absence or presence of red blood cells.
+
+    # pus_cell: Presence of pus cells in urine (int) - Range: Usually represented as binary values (0 or 1) indicating the absence or presence of pus cells.
+
+    # pus_cell_clumps: Presence of clumps of pus cells in urine (int) - Range: Usually represented as binary values (0 or 1) indicating the absence or presence of pus cell clumps.
+
+    # bacteria: Presence of bacteria in urine (int) - Range: Usually represented as binary values (0 or 1) indicating the absence or presence of bacteria.
+
+    # blood_glucose_random: Random blood glucose level (float) - Range: Varies depending on the dataset, typically measured in mg/dL (milligrams per deciliter).
+
+    # blood_urea: Blood urea level (float) - Range: Varies depending on the dataset, typically measured in mg/dL (milligrams per deciliter).
+
+    # serum_creatinine: Serum creatinine level (float) - Range: Varies depending on the dataset, typically measured in mg/dL (milligrams per deciliter).
+
+    # sodium: Sodium level in blood (float) - Range: Varies depending on the dataset, typically measured in mmol/L (millimoles per liter).
+
+    # potassium: Potassium level in blood (float) - Range: Varies depending on the dataset, typically measured in mmol/L (millimoles per liter).
+
+    # haemoglobin: Hemoglobin level in blood (float) - Range: Varies depending on the dataset, typically measured in g/dL (grams per deciliter).
+
+    # packed_cell_volume: Packed cell volume in blood (float) - Range: Varies depending on the dataset, typically measured as a percentage.
+
+    # white_blood_cell_count: White blood cell count (float) - Range: Varies depending on the dataset, typically measured in cells per microliter (mcL).
+
+    # red_blood_cell_count: Red blood cell count (float) - Range: Varies depending on the dataset, typically measured in millions of cells per microliter (mcL).
+
+    # hypertension: Presence of hypertension (int) - Range: Usually represented as binary values (0 or 1) indicating the absence or presence of hypertension.
+
+    # diabetes_mellitus: Presence of diabetes mellitus (int) - Range: Usually represented as binary values (0 or 1) indicating the absence or presence of diabetes mellitus.
+
+    # coronary_artery_disease: Presence or absence of coronary artery disease in the patient (int) - Represents the presence (1) or absence (0) of coronary artery disease in the patient.
+
+    # appetite: Appetite of the patient (int) - Represents the appetite of the patient. It can have the following values:
+
+    # 0: Poor
+    # 1: Good
+
+    # peda_edema: Presence or absence of pedal edema in the patient (int) - Represents the presence (1) or absence (0) of pedal edema in the patient.
+
+    # anemia: Presence or absence of anemia in the patient (int) - Represents the presence (1) or absence (0) of anemia in the patient.
--- app/main.py
+++ app/main.py
@@ -19,8 +19,8 @@ app = FastAPI()
 # Set up CORS
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["*"],
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["*"],
+    # allow_origins=["http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
